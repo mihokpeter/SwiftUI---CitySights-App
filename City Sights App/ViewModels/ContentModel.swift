b80c3470b1fb56766a97d60ff3279213d71ac6ee
Peter Mihok <peter.mihok115@gmail.com>
2021-12-11 21:50:06 +0100

    - create method in ContentModel called getBusiness, where I am creating URL using URLComponents adding queryItems

--- City Sights App/ViewModels/ContentModel.swift
+++ City Sights App/ViewModels/ContentModel.swift
@@ -46,12 +46,59 @@ class ContentModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         // Gives us the location of the user
-        print(locations.first ?? "no location")
+        let userLocation = locations.first
         
-        // TODO: If we have the coordinates of the user, send into Yelp API
+        if userLocation != nil {
+            
+            // We have a location
+            // Stop requesting the location after we get it once
+            locationManager.stopUpdatingHeading()
+            
+            // If we have the coordinates of the user, send into Yelp API
+            //getBusnisses(category: "arts", location: userLocation!)
+            getBusnisses(category: "restaurants", location: userLocation!)
+        }
+    }
+    
+    // MARK: - Yelp API methods
+    
+    func getBusnisses(category:String, location:CLLocation) {
         
-        // Stop requesting the location after we get it once
-        locationManager.stopUpdatingHeading()
+        // Create URL
+        var urlComponents = URLComponents(string: "https://api.yelp.com/v3/businesses/search")
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "latitude", value: String(location.coordinate.latitude)),
+            URLQueryItem(name: "longitude", value: String(location.coordinate.longitude)),
+            URLQueryItem(name: "categories", value: category),
+            URLQueryItem(name: "limit", value: "6")
+        ]
+        
+        let url = urlComponents?.url
+        
+        if let url = url { // Checking if url is actual url, so basically checking if the url is not nil in other words
+            
+            // Create URL Request
+            var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+            request.httpMethod = "GET"
+            request.addValue("Bearer qJBnAG1CunuYR8Lq2mOZzoFFIG2dE3mTHcNG-S49TbQOn3RUgjivRaW6Ftwg1gxkuHmBtXBR9qxFw61PuQHXxx8R0WTdSa1f5Sw7MTqs__NMirDatPEUyXJkcPu0YXYx", forHTTPHeaderField: "Authorization")
+            
+            // Get URL Session
+            let session = URLSession.shared
+            
+            // Create DataTask
+            let dataTask = session.dataTask(with: request) { (data, response, error) in
+                // Check that there is no error
+                if error == nil {
+                    print(response)
+                }
+            }
+            
+            // Start the DataTask
+            dataTask.resume()
+        }
+        
+        
+        
     }
     
 }
