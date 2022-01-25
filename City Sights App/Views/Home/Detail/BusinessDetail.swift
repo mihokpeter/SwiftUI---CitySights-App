c02e8c1869a7888c4682765a02a521d04823342f
Peter Mihok <peter.mihok115@gmail.com>
2022-01-25 20:53:48 +0100

    - DirectionsView with MKMapView - create annotations with directions to the selected business/sight

--- City Sights App/Views/Home/Detail/BusinessDetail.swift
+++ City Sights App/Views/Home/Detail/BusinessDetail.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct BusinessDetail: View {
     
     var business: Business
+    @State private var showDirections = false
     
     var body: some View {
         
@@ -43,21 +44,7 @@ struct BusinessDetail: View {
             
             Group {
                 
-                // Business name
-                Text(business.name!)
-                    .font(.largeTitle)
-                    .padding()
-                
-                // Loop through display address
-                if business.location?.displayAddress != nil {
-                    ForEach(business.location!.displayAddress!, id:\.self) { displayLine in
-                        Text(displayLine)
-                            .padding(.horizontal)
-                    }
-                }
-                
-                // Rating
-                Image("regular_\(business.rating ?? 0)")
+                BusinessTitle(business: business)
                     .padding()
                 
                 Divider()
@@ -102,7 +89,8 @@ struct BusinessDetail: View {
             
             // Get directions button
             Button {
-                // TODO: Show Directions
+                // Show directions
+                showDirections = true
             } label: {
                 ZStack {
                     Rectangle()
@@ -113,7 +101,10 @@ struct BusinessDetail: View {
                         .foregroundColor(.white)
                         .bold()
                 }
-                .padding()
+            }
+            .padding()
+            .sheet(isPresented: $showDirections) {
+                DirectionsView(business: business)
             }
 
             
---
+++ City Sights App/Views/Home/Detail/BusinessTitle.swift
@@ -0,0 +1,35 @@
+//
+//  BusinessTitle.swift
+//  City Sights App
+//
+//  Created by Peter Mihók on 25/01/2022.
+//
+
+import SwiftUI
+
+struct BusinessTitle: View {
+    
+    var business: Business
+    
+    var body: some View {
+        
+        VStack (alignment: .leading) {
+            // Business name
+            Text(business.name!)
+                .font(.largeTitle)
+            
+            // Loop through display address
+            if business.location?.displayAddress != nil {
+                ForEach(business.location!.displayAddress!, id:\.self) { displayLine in
+                    Text(displayLine)
+                }
+            }
+            
+            // Rating
+            Image("regular_\(business.rating ?? 0)")
+        }
+        
+        
+    }
+}
+
